7fd4084155fa1f7e30261746a440f2dbc77c22bb
danshapir <s.dan@live.com>
2020-02-05 18:00:03 +0200

    Add masking support

--- src/logging-interceptor/logging.interceptor.ts
+++ src/logging-interceptor/logging.interceptor.ts
@@ -5,13 +5,23 @@ import {
     NestInterceptor,
   } from '@nestjs/common';
   import { Observable, throwError } from 'rxjs';
-  import { tap, catchError } from 'rxjs/operators';
+  import { tap, catchError, map } from 'rxjs/operators';
 import { IncomingMessage } from 'http';
 import { WinstonLogger } from '@payk/nestjs-winston';
+import * as maskJson from 'mask-json';
   
   @Injectable()
   export class LoggingInterceptor implements NestInterceptor {
     private readonly logger = new WinstonLogger(LoggingInterceptor.name);
+    private readonly jsonMasker = (x: any) => x;
+    /**
+     *
+     */
+    constructor(private readonly maskedFields?: string[]) {
+      if  (maskedFields) {
+        this.jsonMasker = maskJson(maskedFields);
+      }
+    }
 
     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
       if (context.getType() === 'http') {
@@ -24,17 +34,17 @@ import { WinstonLogger } from '@payk/nestjs-winston';
 
     handleHttp(context: ExecutionContext, next: CallHandler): Observable<any> {
       const { body, params, query, headers, method, url, route } = context.getArgs().find((a) => a instanceof IncomingMessage )
-        this.logger.info(`Request: To path: ${route.path} (${url}) with method ${method}`, { body, params, query, headers, method, url, path: route.path} )
+        this.logger.info(`Request: To path: ${route.path} (${url}) with method ${method}`,  this.jsonMasker({ body, params: params, query, headers, method, url, path: route.path }) )
   
         return next
           .handle()
           .pipe(tap((response) =>  {
             let httpResponse = context.switchToHttp().getResponse();
-            this.logger.info(`Response: To path: ${route.path} (${url}) with method ${method}, status code: ${httpResponse.statusCode}`, { response: { headers: httpResponse.getHeaders(), statusCode: httpResponse.statusCode, body: response }, request: { body, params, query, headers, method, url, path: route.path }})
+            this.logger.info(`Response: To path: ${route.path} (${url}) with method ${method}, status code: ${httpResponse.statusCode}`, this.jsonMasker({ response: { headers: httpResponse.getHeaders(), statusCode: httpResponse.statusCode, body: response }, request: { body, params, query, headers, method, url, path: route.path }}))
           })).pipe(catchError((err) => {
             let httpResponse = context.switchToHttp().getResponse();
             let statusCode = err?.status;
-            this.logger.error(`Error Response: To path: ${route.path} (${url}) with method ${method}, status code: ${statusCode ?? httpResponse.statusCode}`, { response: { headers: httpResponse.getHeaders(), statusCode: httpResponse.statusCode, error: err }, request: { body, params, query, headers, method, url, path: route.path }})
+            this.logger.error(`Error Response: To path: ${route.path} (${url}) with method ${method}, status code: ${statusCode ?? httpResponse.statusCode}`, this.jsonMasker({ response: { headers: httpResponse.getHeaders(), statusCode: httpResponse.statusCode, error: err }, request: { body, params, query, headers, method, url, path: route.path }}))
             return throwError(err)
           }));
     }
@@ -43,13 +53,13 @@ import { WinstonLogger } from '@payk/nestjs-winston';
       const rpcContext = context.switchToRpc();
       const data = rpcContext.getData();
 
-      this.logger.info(`Request: Of type: ${data.type} with process id ${data.bpmnProcessId} `, { ...data })
+      this.logger.info(`Request: Of type: ${data.type} with process id ${data.bpmnProcessId} `, this.jsonMasker({ ...data }));
       let nextHn = next.handle();
       let n2 = nextHn.pipe(catchError((err) => {
-        this.logger.error(`Error Response: Of type: ${data.type} with process id ${data.bpmnProcessId} `, { request: data, error: err });
+        this.logger.error(`Error Response: Of type: ${data.type} with process id ${data.bpmnProcessId} `, this.jsonMasker({ request: data, error: err }));
         return throwError(err);
       })).toPromise().then((res) => {
-          this.logger.info(`Response: Of type: ${data.type} with process id ${data.bpmnProcessId} `, { request: data, response: res });
+          this.logger.info(`Response: Of type: ${data.type} with process id ${data.bpmnProcessId} `, this.jsonMasker({ request: data, response: res }));
           return res;
       }).catch((err) => {
         throw err;
